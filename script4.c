e8cbb2dbd3aff30fe2d909ef2628d6691f00e52a
Nurgaleev_11908 <55531380+Mansur908@users.noreply.github.com>
2021-11-27 03:19:58 +0300

    Create script4.c

---
+++ script4.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(int argc , char *argv[]) {
+        FILE *fout;
+        fout=fopen(argv[1], "r");
+        FILE *fin;
+        fin=fopen(argv[2], "wt");
+        if (!fout || !fin) {
+                printf("%s", "File open error\n");
+                return 1;
+        }
+        int s;
+        while ((s=getc(fout)) != EOF) {
+            fputc(s, fin);
+        }
+
+        if (fclose(fout) == -1) {
+                printf("%s", "File close error\n");
+                return 1;
+        }
+        return 0;
+}
